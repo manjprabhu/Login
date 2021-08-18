cdb95ff92ae000c99911ac6c4c82fade4ad28952
MPB <mp4learning@gmail.com>
2021-08-18 21:07:07 +0530

    Code cleanup

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -7,7 +7,7 @@ import com.example.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mBinding: ActivityMainBinding
+    private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
